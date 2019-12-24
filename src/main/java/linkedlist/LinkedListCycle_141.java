863aa5191ee851c4d504c3252b76c571e817061c
Rock_Cao <callmecy@163.com>
2019-12-25 01:27:29 +0800

    141 LinkedListCycle by two points

---
+++ src/main/java/linkedlist/LinkedListCycle_141.java
@@ -0,0 +1,39 @@
+package linkedlist;
+
+import java.util.HashSet;
+
+/**
+ * @author ：Rock-Cao
+ * @date ：Created at 2019/12/25 0:20
+ * @modified By：
+ * @version: 1.0$
+ */
+public class LinkedListCycle_141 {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            } else {
+                set.add(head);
+                head = head.next;
+            }
+        }
+        return false;
+    }
+    public boolean hasCycleByTwoPointers(ListNode head) {
+        if (head == null||head.next==null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
--- src/main/java/linkedlist/ReverseLinkedList_206.java
+++ src/main/java/linkedlist/ReverseLinkedList_206.java
@@ -10,12 +10,11 @@ package linkedlist;
 public class ReverseLinkedList_206 {
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
-        ListNode cur = head;
-        while (cur!= null) {
-            ListNode next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
+        while (head!= null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
         }
         return pre;
     }
